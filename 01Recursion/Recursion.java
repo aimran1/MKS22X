85fac3b944d99cb19fc15670714baf67b9575b88
ahmed <aimran@stuy.edu>
2018-02-03 20:25:45 -0500

    Tested factorial method on negatives

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -15,14 +15,14 @@ public class Recursion{
   }
 
   public static void main(String[] args){
-    System.out.println(fact(0));
-    System.out.println(fact(1));
-    System.out.println(fact(2));
-    System.out.println(fact(3));
-    System.out.println(fact(4));
-    System.out.println(fact(5));
-    System.out.println(fact(6));
-    System.out.println(fact(7));
+    System.out.println(fact(-100));
+    // System.out.println(fact(1));
+    // System.out.println(fact(2));
+    //  System.out.println(fact(3));
+    // System.out.println(fact(4));
+    //  System.out.println(fact(5));
+    //  System.out.println(fact(6));
+    // System.out.println(fact(7));
   }
 
 }
