5b9bbf2db9950fac65b3ebadfe554773f18af220
aimran <aimran@stuycs.org>
2018-02-12 15:53:57 -0500

    Added solve

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -6,60 +6,64 @@ public class QueenBoard{
 	board = new int[size][size];
     }
     
-  private boolean addQueen(int r, int c){
-    if (board[r][c] == 0){
+    private boolean addQueen(int r, int c){
+	if (board[r][c] == 0){
 	    board[r][c] = -1;
-      for (int row = 0; row < board.length; row++){
-        for (int col = 0; col < board[row].length; col++){
-            if (row == r && col != c){
-              board[row][col] += 1; 
-            }
-            if (col == c && row != r){
-              board[row][col] += 1;
-            }
-            if ((c - col == r - row) && (row != r || col != c)){
-              board[row][col] += 1;
-            }
-        }
-      }
-      for (int i = 1; r + i < board.length || c + i < board.length; i++){
-        board[r+i][c-i]+=1;
-        if(r-i>=0 && c+i <board.length){
-          board[r-i][c+i]+=1;
-        }
-      }
-      return true;
-    }
-    return false;
-  }
-
-    private boolean removeQueen(int r, int c){
-	if (board[r][c] < 0){
-	    board[r][c] = 0;
-       for (int row = 0; row < board.length; row++){
-        for (int col = 0; col < board[row].length; col++){
-            if (row == r && col != c){
-              board[row][col] -= 1; 
-            }
-            if (col == c && row != r){
-              board[row][col] -= 1;
-            }
-            if ((c - col == r - row) && (row != r || col != c)){
-              board[row][col] -= 1;
-            }
-        }
-      }
-      for (int i = 1; r + i < board.length || c + i < board.length; i++){
-        board[r+i][c-i]-=1;
-        if(r-i>=0 && c+i <board.length){
-          board[r-i][c+i]-=1;
-        }
-      }
+	    for (int row = 0; row < board.length; row++){
+		for (int col = 0; col < board[row].length; col++){
+		    if (row == r && col != c){
+			board[row][col] += 1; 
+		    }
+		    if (col == c && row != r){
+			board[row][col] += 1;
+		    }
+		    if ((c - col == r - row) && (row != r || col != c)){
+			board[row][col] += 1;
+		    }
+		}
+	    }
+	    for (int i = 1; r + i < board.length || c + i < board.length; i++){
+		if( r + i < board.length && c - i >= 0){ 
+		    board[r+i][c-i]+=1;
+		}
+		if(r-i>=0 && c+i <board.length){
+		    board[r-i][c+i]+=1;
+		}
+	    }
 	    return true;
 	}
 	return false;
     }
-
+    
+    private boolean removeQueen(int r, int c){
+	if (board[r][c] < 0){
+	    board[r][c] = 0;
+	    for (int row = 0; row < board.length; row++){
+		for (int col = 0; col < board[row].length; col++){
+		    if (row == r && col != c){
+			board[row][col] -= 1; 
+		    }
+		    if (col == c && row != r){
+			board[row][col] -= 1;
+		    }
+		    if ((c - col == r - row) && (row != r || col != c)){
+			board[row][col] -= 1;
+		    }
+		}
+	    }
+	    for (int i = 1; r + i < board.length || c + i < board.length; i++){
+		if( r + i < board.length && c - i >= 0){ 
+		    board[r+i][c-i]-=1;
+		}
+		if(r-i>=0 && c+i <board.length){
+		    board[r-i][c+i]-=1;
+		}
+	    }
+	    return true;
+	}
+	return false;
+    }
+    
     public String toString(){
 	String b = "";
 	for (int i = 0; i < board.length; i++){
@@ -75,13 +79,34 @@ public class QueenBoard{
 	return b;
     }
 
-    public static void main (String[] args){
-      QueenBoard n = new QueenBoard(5);
-       n.addQueen(1,3);
-      // n.addQueen(2,4);
-      	n.removeQueen(1,3);
-      //	n.removeQueen(1,3);
-      System.out.println(n);
+    public boolean solve(){
+	return help(0);
     }
 
+    public boolean help(int col){
+	if (col > board.length-1){
+	    return true;
+	}
+	for ( int i = 0; i < board.length; i++){
+	    if (addQueen(i,col)){
+		if (help(col+1)){
+		    return true;
+		}
+		removeQueen(i,col);
+	    }
+	}
+	return false;
+    }
+    
+    public static void main (String[] args){
+	QueenBoard n = new QueenBoard(10);
+	n.solve();
+	// n.addQueen(2,4);
+	// n.removeQueen(1,3);
+	// n.removeQueen(1,3);
+	System.out.println(Text.go(1,1));
+	System.out.println(this);
+	Text.wait(50);
+    }
+    
 }
