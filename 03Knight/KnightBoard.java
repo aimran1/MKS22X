24954d7143e143bdab8ecc3c5c7de1f0c01a2ba2
aimran <aimran@stuycs.org>
2018-02-13 15:54:09 -0500

    Working on solve

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -28,5 +28,33 @@ public class KnightBoard{
 	    }
 	}
     }
+
+    public boolean solve(int startingRows, int startingCol){
+
+	if (!zero()){
+	    throw new IllegalStateException();
+	}
+	if (startingRows < 0 || startingCols < 0 ||
+	    startingRows > board.length || startingCol > board[0].length){
+	    throw new IllegalArgumentException();
+	}
+	
+	return solveH(0,0,1);
+    }
+
+    public boolean solveH(int row, int col, int level){
+	
+    }
+
+    public boolean zero(){
+	for (int i = 0; i < board.length; i++){
+	    for (int j = 0; j < board[i].length; j++){
+		if (board[i][j] != 0){
+		    return false;
+		}
+	    }
+	}
+	return true;
+    }
     
 }
