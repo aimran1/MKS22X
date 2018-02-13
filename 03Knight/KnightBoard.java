a19810a3656510fdf98e4ccdf6adf1ec41559a1d
aimran <aimran@stuycs.org>
2018-02-13 15:42:53 -0500

    Created constructor

---
+++ 03Knight/KnightBoard.java
@@ -0,0 +1,17 @@
+public class KnightBoard{
+
+    private int[][] board;
+    
+    public KnightBoard(int startingRows, int startingCols){
+	if (startingRows < 0 || startingCols < 0){
+	    throw new IllegalArgumentException();
+	}
+	board = new int[startingRows][startingCols];
+	for (int i = 0; i < board.length; i++){
+	    for (int j = 0; j < board[i].length; j++){
+		board[i][j] = 0; 
+	    }
+	}
+    }
+    
+}
