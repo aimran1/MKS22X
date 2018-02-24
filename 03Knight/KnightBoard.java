01f85fd033a37ae3d520486592bae08837c357f6
ahmed <aimran@stuy.edu>
2018-02-23 21:00:50 -0500

    Lots of edits and new incrementing method for add, but still errors with backtrakcing

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -2,6 +2,8 @@ public class KnightBoard{
   
   private int[][] board;
   private int[][] cboard;
+  private int[] x ={1,1,2,2,-1,-1,-2,-2};
+  private int[] y ={2,-2,1,-1,2,-2,-1,1};
   
   public KnightBoard(int startingRows, int startingCols){
     if (startingRows < 0 || startingCols < 0){
@@ -13,7 +15,7 @@ public class KnightBoard{
         board[i][j] = 0; 
 	    }
     }
-    cboard = board;
+     cboard = board;
   }
 
   public String toString(){
@@ -21,11 +23,11 @@ public class KnightBoard{
     for (int i = 0; i < board.length; i++){
 	    b += "\n";
 	    for (int j = 0; j < board[i].length; j++){
-        if (board[j][i] < 10){
-          b += "  " + board[j][i];
+        if (board[i][j] < 10){
+          b += "  " + board[i][j];
         }
         else{
-          b += " " + board[j][i];
+          b += " " + board[i][j];
         }
 	    }
     }
@@ -34,48 +36,55 @@ public class KnightBoard{
 
   public boolean solve(int startingRows, int startingCol){
     
-    if (!zero()){
-	    throw new IllegalStateException();
-    }
-    if (startingRows < 0 || startingCol < 0 ||
+    if (!zero() ||
+        startingRows < 0 || startingCol < 0 ||
         startingRows > board.length || startingCol > board[0].length){
 	    throw new IllegalArgumentException();
     }
-	
-    return solveH(0,0,1);
+    return solveH(startingRows,startingCol,1);
   }
 
   public boolean solveH(int row, int col, int level){
-    if (level > board.length * board[0].length){
+    if (level ==  board.length * board[0].length){
+      board[row][col] = level;
       return true;
     }
-    for(int i = 0; i < board.length; i++){
-      for (int j = 0; j < board[i].length; j++){
-        if (isValid(row,col,i,j)){
-          board[i][j] = level;
-          solveH(i,j,level + 1);
+    for (int i = 0; i < 8; i++){
+      if(test(row+x[i],col+y[i],level)){
+        if(solveH(row+x[i],col+y[i],level+1)){
+          return true;
         }
+        board[row][col] = 0;
       }
     }
-    board = cboard;
+    /* if(test(row,col,level)){
+      return solveH(row+x[0],col+y[0],level+1) ||
+        solveH(row+x[1],col+y[1],level+1) ||
+        solveH(row+x[2],col+y[2],level+1) ||
+        solveH(row+x[3],col+y[3],level+1) ||
+        solveH(row+x[4],col+y[4],level+1) ||
+        solveH(row+x[5],col+y[5],level+1) ||
+        solveH(row+x[6],col+y[6],level+1) ||
+        solveH(row+x[7],col+y[7],level+1);       
+        }*/
     return false;
   }
 
-  public boolean isValid(int row, int col, int newR, int newC){
-    if (board[newR][newC] > 0){
-	    return false;
-    }
-    if (newR >= board.length || newR < 0 ||
-        newC >= board[0].length || newC < 0 ||
-        row >= board.length || row < 0 ||
-        col >= board.length || col < 0){
-	    return false;
-    }
-    if (Math.abs(newR - row) == 2 && Math.abs(newC - col) == 1 ||
-        Math.abs(newC - col) == 2 && Math.abs(newR - row) == 1){
+  public boolean test(int newr, int newc, int level){
+    if (isValid(newr,newc)){
+      board[newr][newc] = level;
       return true;
     }
     return false; 
+  }
+  
+  private boolean isValid(int newR, int newC){
+    if (newR >= 0 && newR < board.length &&
+        newC < board[0].length && newC >= 0 &&
+        board[newR][newC] == 0){
+	    return true;
+    }
+    return false;
   }
 
   public boolean zero(){
@@ -90,9 +99,9 @@ public class KnightBoard{
   }
 
   public static void main(String[] args){
-    KnightBoard b = new KnightBoard(5,5);
-    System.out.println(b);
-    b.solve(0,0);
+    KnightBoard b = new KnightBoard(3,5);
+    System.out.println(b.solve(0,0));
+    // b.add(6,5,10);
     System.out.println(b);
   }
     
