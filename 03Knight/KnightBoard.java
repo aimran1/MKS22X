2f89fcccd7827b79c9fa89eb8cb64515d5b5004c
ahmed <aimran@stuy.edu>
2018-02-24 20:57:40 -0500

    Further tested solve

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -23,7 +23,10 @@ public class KnightBoard{
     for (int i = 0; i < board.length; i++){
 	    b += "\n";
 	    for (int j = 0; j < board[i].length; j++){
-        if (board[i][j] < 10){
+        if(board[i][j] == 0){
+          b += " _";
+        }
+        else if (board[i][j] < 10){
           b += "  " + board[i][j];
         }
         else{
@@ -35,8 +38,11 @@ public class KnightBoard{
   }
 
   public boolean solve(int startingRows, int startingCol){
-    if (!isValid(startingRows,startingCol)){
-	    throw new IllegalArgumentException();
+    if (!zero()){
+	    throw new IllegalStateException();
+    }
+    if(!isValid(startingRows,startingCol)){
+      throw new IllegalArgumentException();
     }
     return solveH(startingRows,startingCol,1);
   }
@@ -62,12 +68,12 @@ public class KnightBoard{
     if (newR >= 0 && newR < board.length &&
         newC < board[0].length && newC >= 0 &&
         board[newR][newC] == 0){
-	    return true;
+      return true;
     }
     return false;
   }
 
-  public boolean zero(){
+  private boolean zero(){
     for (int i = 0; i < board.length; i++){
 	    for (int j = 0; j < board[i].length; j++){
         if (board[i][j] != 0){
@@ -79,11 +85,84 @@ public class KnightBoard{
   }
   
   public static void main(String[] args){
-    KnightBoard b = new KnightBoard(5,5);
-    System.out.println(b.solve(0,0));
-    
-    // b.add(6,5,10);
-    System.out.println(b);
+    // KnightBoard a = new KnightBoard(3,3);
+
+    //  KnightBoard b = new KnightBoard(5,5);
+    // System.out.println(b.solve(0,0)); //prints true
+    // System.out.println(b); //prints a valid solution
+
+    /*try{
+      b.solve(0,0);
+    }catch(IllegalStateException e){
+      System.out.println("Error: The board contains non-zero values");
+    } //prints "Error: The board contains non-zero values"
+    */
+    /* try{
+      b.countSolutions(0,0);
+    }catch(IllegalStateException e){
+      System.out.println("Error: The board contains non-zero values");
+    } //prints "Error: The board contains non-zero values"
+    */
+    /*  try{
+      KnightBoard b1 = new KnightBoard(-1,0);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters in the constructor");
+    } //prints "Error: There cannot be negative parameters in the constructor"
+    */
+    /* try{
+      KnightBoard b1 = new KnightBoard(1,-1);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters in the constructor");
+    } //prints "Error: There cannot be negative parameters in the constructor"
+    */
+    /*    try{
+      KnightBoard b1 = new KnightBoard(-1,-1);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters in the constructor");
+    } //prints "Error: There cannot be negative parameters in the constructor"
+    */
+    /*  try{
+      KnightBoard b1 = new KnightBoard(5,5);
+      b1.solve(0,-1);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters");
+    } //prints "Error: There cannot be negative parameters"
+    /    */
+    /*  try{
+      KnightBoard b1 = new KnightBoard(5,5);
+      b1.solve(-1,0);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters");
+    } //prints "Error: There cannot be negative parameters"
+    */
+    /*  try{
+      KnightBoard b1 = new KnightBoard(5,5);
+      b1.solve(-1,-1);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters");
+    } //prints "Error: There cannot be negative parameters"
+    */
+
+
+    /* for (int i = 0; i < 5; i++){
+      for (int j = 0; j < 5; j++){
+        KnightBoard abc = new KnightBoard(5,5);
+        System.out.println(abc.solve(i,j)); //prints alternating lines of true/false starting with true
+      }
+      }*/
+    KnightBoard c = new KnightBoard(5,5);
+
+    int totalSol = 0;
+    /* for (int i = 0; i < 5; i++){
+      for (int j = 0; j < 5; j++){
+        totalSol += c.countSolutions(i,j);
+      }
+      }*/
+
+    //  System.out.println(totalSol); //prints 1728
+
+    /* KnightBoard d = new KnightBoard(5,5);
+   System.out.println(d.countSolutions(0,0)); //prints 304*/
+
   }
-    
   }
