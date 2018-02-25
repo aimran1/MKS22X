45fb085ed1bcf579bc69e78e4e2f65f9dc53a5a4
ahmed <aimran@stuy.edu>
2018-02-24 20:21:42 -0500

    Works for 5x5 board

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -35,47 +35,27 @@ public class KnightBoard{
   }
 
   public boolean solve(int startingRows, int startingCol){
-    
-    if (!zero() ||
-        startingRows < 0 || startingCol < 0 ||
-        startingRows > board.length || startingCol > board[0].length){
+    if (!isValid(startingRows,startingCol)){
 	    throw new IllegalArgumentException();
     }
     return solveH(startingRows,startingCol,1);
   }
 
   public boolean solveH(int row, int col, int level){
-    if (level ==  board.length * board[0].length){
+    if (level == board.length * board[0].length){
       board[row][col] = level;
       return true;
     }
     for (int i = 0; i < 8; i++){
-      if(test(row+x[i],col+y[i],level)){
+      if(isValid(row+x[i],col+y[i])){
+        board[row][col] = level;
         if(solveH(row+x[i],col+y[i],level+1)){
           return true;
         }
         board[row][col] = 0;
       }
     }
-    /* if(test(row,col,level)){
-      return solveH(row+x[0],col+y[0],level+1) ||
-        solveH(row+x[1],col+y[1],level+1) ||
-        solveH(row+x[2],col+y[2],level+1) ||
-        solveH(row+x[3],col+y[3],level+1) ||
-        solveH(row+x[4],col+y[4],level+1) ||
-        solveH(row+x[5],col+y[5],level+1) ||
-        solveH(row+x[6],col+y[6],level+1) ||
-        solveH(row+x[7],col+y[7],level+1);       
-        }*/
     return false;
-  }
-
-  public boolean test(int newr, int newc, int level){
-    if (isValid(newr,newc)){
-      board[newr][newc] = level;
-      return true;
-    }
-    return false; 
   }
   
   private boolean isValid(int newR, int newC){
@@ -97,12 +77,13 @@ public class KnightBoard{
     }
     return true;
   }
-
+  
   public static void main(String[] args){
-    KnightBoard b = new KnightBoard(3,5);
+    KnightBoard b = new KnightBoard(5,5);
     System.out.println(b.solve(0,0));
+    
     // b.add(6,5,10);
     System.out.println(b);
   }
     
-}
+  }
