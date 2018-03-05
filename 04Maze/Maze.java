a6920226bf4334894e243913708d6977376cd7b9
ahmed <aimran@stuy.edu>
2018-03-04 22:49:51 -0500

    Fixed scanner and added board check

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -2,76 +2,109 @@ import java.util.*;
 import java.io.*;
 
 public class Maze{
-    private char[][]maze;
-    private boolean animate;
-    private int rows = 0;
-    private int cols = 0;
+  private char[][]maze;
+  private boolean animate;
+  private int[] sLoc = new int[2];
+  private int rows = 0;
+  private int cols = 0;
     
-    public Maze(String filename) throws FileNotFoundException{
-    	File text = new File(filename + ".dat");
-	Scanner in = new Scanner(text);
-	String line = "";
-	while (in.hasNextLine()){
-	    line += in.nextLine();
+  public Maze(String filename) throws FileNotFoundException{
+    File text = new File(filename);
+    Scanner in = new Scanner(text);
+    Scanner m = new Scanner(text);
+    String line = "";
+    while (in.hasNextLine()){
+	    line = in.nextLine();
 	    cols = line.length();
 	    rows++;
-	}
-	maze = new char[rows][cols];
-	
-	System.out.println(toString());
     }
-
-    private void wait(int millis){
-         try {
-             Thread.sleep(millis);
-         }
-         catch (InterruptedException e) {
-         }
-     }
-
-
-    public void setAnimate(boolean b){
-
-        animate = b;
-
+    maze = new char[rows][cols];
+    for (int i = 0; m.hasNextLine(); i++){
+      String c = m.nextLine();
+      for(int j = 0; j < c.length(); j++){
+        maze[i][j] = c.charAt(j);
+      }
     }
+    isValid();
+    System.out.println(toString());
+  }
 
+  public int solve(){
+    return 1;
+  }
 
-    public void clearTerminal(){
-
-        //erase terminal, go to top left of screen.
-
-        System.out.println("\033[2J\033[1;1H");
-
+  public boolean isValid(){
+    int s = 0;
+    int e = 0;
+    for (int i = 0; i < maze.length; i++){
+      for (int j = 0; j < maze[i].length; j++){
+        if(maze[i][j] == 'S'){
+          s++;
+          sLoc[0] = i;
+          sLoc[1] = j;
+        }
+        if(maze[i][j] == 'S'){
+          e++;
+        }
+      }
     }
+    if (s != 1 && e != 1){
+      throw New IllegalStateException();
+    }
+    return true;
+  }
+  
+  private void wait(int millis){
+    try {
+      Thread.sleep(millis);
+    }
+    catch (InterruptedException e) {
+    }
+  }
+  
 
-    private void maker(String s){
-	for (int i = 0; i < s.length()-1;i++,cols++){
+  public void setAnimate(boolean b){
+    
+    animate = b;
+    
+  }
+
+
+  public void clearTerminal(){
+    
+    //erase terminal, go to top left of screen.
+    
+    System.out.println("\033[2J\033[1;1H");
+
+  }
+
+  private void maker(String s){
+    for (int i = 0; i < s.length()-1;i++,cols++){
 	    if (s.charAt(i) == '\n'){
-		rows++;
-		cols = 0;
+        rows++;
+        cols = 0;
 	    }else{
-		maze[rows][cols] = s.charAt(i);
+        maze[rows][cols] = s.charAt(i);
 	    }
-	}
     }
+  }
 
-    public String toString(){
-	String ans = "";
-	for (int i = 0; i < rows-1;i++){
-	    for (int j = 0; j < cols-1; j++){
-		ans += maze[i][j];
+  public String toString(){
+    String ans = "";
+    for (int i = 0; i < rows;i++){
+	    for (int j = 0; j < cols; j++){
+        ans += maze[i][j];
 	    }
 	    ans += '\n'; 
-	}
-	return ans;
-      }
-
-    public static void main(String[] args) {
-	try{
-	    Maze m = new Maze("d1");    
-	}catch(FileNotFoundException e){
-	    System.out.println("Are u sure the file's here");
-	}
     }
+    return ans;
+  }
+  
+  public static void main(String[] args) {
+    try{
+	    Maze m = new Maze("d1.dat");    
+    }catch(FileNotFoundException e){
+	    System.out.println("Are u sure the file's here");
+    }
+  }
 }
