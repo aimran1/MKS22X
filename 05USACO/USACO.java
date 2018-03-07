b37d60109e567a160e1ca6345648cd07731e73b6
aimran <aimran@stuycs.org>
2018-03-07 15:55:26 -0500

    Working

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -3,23 +3,32 @@ import java.io.*;
 
 public class USACO{
 
-  public static int bronze(String filename) throws FileNotFoundException{
-    File l = new File(filename);
-    Scanner in = new Scanner(l);
-    // String line = in.nextLine();
-    int R, C, E, N;
-    R = in.nextInt();
-    C = in.nextInt();
-    E = in.nextInt();
-    N = in.nextInt();
-    int[][] lake = new int[R][C];
-    for (int i = 0; i < R; i++){
-      for (int j = 0; j < C; j++){
-	  lake[R][C] = in.nextInt();
-      }
+    public static int bronze(String filename) throws FileNotFoundException{
+	File l = new File(filename);
+	Scanner in = new Scanner(l);
+	// String line = in.nextLine();
+	int R, C, E, N;
+	R = in.nextInt();
+	C = in.nextInt();
+	E = in.nextInt();
+	N = in.nextInt();
+	int[][] lake = new int[R][C];
+	for (int i = 0; i < R; i++){
+	    for (int j = 0; j < C; j++){
+		lake[R][C] = in.nextInt();
+	    }
+	}
+	System.out.println("R:" + R + 
+			   ";C:" + C +
+			   ";E:" + E +
+			   ";N:" + N);
+	return 1;
     }
-    System.out.println("R:");
-    return 1;
-  }
+    
+    public static void main (String[] args){
+	try{
+        bronze("d1.txt");
+	}catch(FileNotFoundException e){}
+    }
 
 }
