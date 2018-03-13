bfd204e36e87767ce1e9aced213778ceedb7bb54
ahmed <aimran@stuy.edu>
2018-03-12 21:06:25 -0400

    Random bounds work

--- 06Part/Partition.java
+++ 06Part/Partition.java
@@ -4,8 +4,15 @@ import java.io.*;
 public class Partition{
 
   public static int partition(int[]data,int start,int end){
-    Random ind;
+    Random ind = new Random();
     int index = ind.nextInt((end-start)+1) + start;
+    System.out.println(index);
+    return index;
+  }
+
+  public static void main(String[] args){
+    int[] d = {2,12,54,5,8,21,4,545,4,854};
+    partition(d,3,7);
   }
 
 }
