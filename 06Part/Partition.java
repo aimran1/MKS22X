193e5301b1a3558c43503b930a6258298302759f
ahmed <aimran@stuy.edu>
2018-03-12 21:01:15 -0400

    Started Partition sort code

---
+++ 06Part/Partition.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Partition{
+
+  public static int partition(int[]data,int start,int end){
+    Random ind;
+    int index = ind.nextInt((end-start)+1) + start;
+  }
+
+}
