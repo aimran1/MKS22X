3b39bbe4c727b6ba614ea49eb72d95754750ce2a
ahmed <aimran@stuy.edu>
2018-03-14 22:56:35 -0400

    Fixing to incorporate equal numbers

--- 06Part/Partition.java
+++ 06Part/Partition.java
@@ -6,21 +6,30 @@ public class Partition{
   public static int partition(int[]data,int start,int end){
     Random ind = new Random();
     int index = ind.nextInt((end-start)+1) + start;
+    int equal = 1;
     swap(data,0,index);
 
     end = data.length - 1;
     while (start <= end){
-      if (data[start] <= data[0]){
+      if (data[start] == data[0] && start == equal){
+        start++;
+      }
+      else if (data[start] == data[0]){
+        swap(data,equal,start);
+        start++;
+      }
+      else if (data[start] < data[0]){
         start++;
       }
       else if (data[start] > data[0]){
         swap(data,start,end);
         end--;
       }
-      //    System.out.println(Arrays.toString(data));
+      System.out.println(Arrays.toString(data));
     }
 
     swap(data,0,end);
+      System.out.println(Arrays.toString(data));
 
     return end;
   }
@@ -31,10 +40,10 @@ public class Partition{
     ary[b] = c;
   }
 
-  /*  public static void main(String[] args){
+  public static void main(String[] args){
     int[] d = {2,12,54,5,8,21,4,545,4,854,12354,2,
                -94,25,3};
     partition(d,3,7);
-    }*/
+  }
 
 }
