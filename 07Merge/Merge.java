9e174e7827589f75150d9e40a273989339618a1c
aimran <aimran@stuycs.org>
2018-03-20 15:57:33 -0400

    Wrote three functions

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -34,6 +34,45 @@ public class Merge{
     return m;
   }
 
+    public static void mergesort(int[] data){
+	msort(data, new int[data.length], 0, data.length-1);
+    }
+
+    private static void msort(int[] data, int[] temp, int lo, int hi){
+	if (lo >= hi){
+	    return;
+	}
+	temp = data;
+	int mid = (hi - lo)/2 + lo;
+	msort(temp,data,lo,mid);
+	msort(temp,data,mid+1,hi);
+	merge(data,temp,lo,mid,mid+1,hi);
+    }
+
+    private static void merge(int[]data,int[]temp,int lo, int mid, int Umid, int hi){
+	int[] m = new int[data.length+temp.length];
+	int i = 0;
+	while(low <= mid && Umid <= hi){
+	    if (data[low] < temp[Umid]){
+		m[i] = data[low];
+		i++;
+		low++;
+	    }
+	    else if (data[low] > temp[Umid]){
+		m[i] = temp[Umid];
+		i++;
+		Umid++;
+	    }
+	}
+	if (low != mid){
+	    m[i] = data[mid];
+	}
+	else if (Umid != hi){
+	    m[i] = temp[hi];
+	}
+    }
+    
+
   public static void main(String[] args){
     int[] l = {1,34,3,455,2,234,2,5,6};
     int[] d = {2,34,34,5,6,87,34,2,78};
