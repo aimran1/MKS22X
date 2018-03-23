c6c77a6e069e7b6837b8ae09aa515c7ed5d0e714
Ahmed.Imran <aimran@stuy.edu>
2018-03-22 22:21:32 -0400

    Progress but some mroe to debug

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -8,46 +8,38 @@ public class Merge{
   }
 
   private static void msort(int[] data, int[] temp, int lo, int hi){
-    if (lo >= hi){
-	    return;
-    }
-    temp = data;
-    int mid = (hi - lo)/2 + lo;
-    msort(temp,data,lo,mid);
-    msort(temp,data,mid+1,hi);
-    merge(data,temp,lo,mid,mid+1,hi);
+      int mid = (hi + lo)/2;
+      
+      if (lo < hi){
+	  for (int i = lo; i <= hi;i++){
+	      temp[i] = data[i];
+	  }
+	  msort(temp,data,lo,mid);
+	  msort(temp,data,mid+1,hi);
+      }
+      merge(data,temp,lo,mid,mid+1,hi);
   }
 
-  private static void merge(int[]data,int[]temp,int lo, int mid, int Umid, int hi){
-    int[] m = new int[data.length+temp.length];
-    int i = lo;
-    for(;lo <= mid; i++){
-	if (temp[lo] <= temp[mid]){
-	    data[i] = temp[lo];
-	    lo++;
-	}
-	else {
-	    data[i] = temp[mid];
-	    mid--;
-	}
-    }
-    for(;Umid <= hi;i++){
-	if(temp[Umid] >= temp[hi]){
-	    data[i] = temp[hi];
-	    lo++;
-	}
-	else{
-	    data[i] = temp[Umid];
-	    Umid--;
+    private static void merge(int[]data,int[]temp,int lo, int mid, int Umid, int hi){
+    for(int i = lo;i < hi; i++){
+	if(lo <= mid){
+	    if (temp[lo] < temp[Umid]){
+		data[i] = temp[lo];
+		lo++;
+	    }
+	    else {
+		data[i] = temp[Umid];
+		Umid++;
+	    }
 	}
     }
     
-    System.out.println(Arrays.toString(m));
+    System.out.println(Arrays.toString(data));
   }
     
 
   public static void main(String[] args){
-    int[] l = {1,34,3,455,2,234,2,5,6};
+      // int[] l = {1,34,3,455,2,234,2,5,6};
     int[] d = {2,34,34,5,6,87,34,2,78};
     // mergesort(l);
     // System.out.println(Arrays.toString(l));
