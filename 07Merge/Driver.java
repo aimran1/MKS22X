0d4beeae8a723f371c9a944a068287849c4dba7d
Ahmed.Imran <aimran@stuy.edu>
2018-03-22 23:14:30 -0400

    Tested Merge and it works

--- 07Merge/Driver.java
+++ 07Merge/Driver.java
@@ -69,7 +69,7 @@ public class Driver{
     /*
      * Test your sort here!
      */
-   Merge. mergesort(start);
+   Merge.mergesort(start);
     long elapsedTime = System.currentTimeMillis() - startTime;
     if(Arrays.equals(start,result)){
       System.out.println("PASS Case "+name(type)+" array, size:"+size+" "+elapsedTime/1000.0+"sec ");
