6ed7c0f378f6a42381362305ec9648c0597182fd
aimran <aimran@stuycs.org>
2018-03-28 15:55:27 -0400

    wrote set(index, n) for list class

--- 08Link/MyLinkedList.java
+++ 08Link/MyLinkedList.java
@@ -9,13 +9,21 @@ public class MyLinkedList{
 	    if (i == n){
 		return c.getValue();
 	    }
-	    c = first.next();
+	    c = c.next();
 	}
 	return -1;
     }
 
-    
-    
+    public void set(int index, int value){
+	Node c = first;
+	for (int i = 0; i < length; i++){
+	    if (i == index){
+		c.setValue(value);
+	    }
+	    c = c.next{};
+	}
+    }
+
 }
 
 private class Node{
