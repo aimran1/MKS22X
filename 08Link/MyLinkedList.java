fd40c8fc622d2efcc63673d230455a2f9cfa3f9c
aimran <aimran@stuycs.org>
2018-03-29 15:51:37 -0400

    Compiles

--- 08Link/MyLinkedList.java
+++ 08Link/MyLinkedList.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class MyLinkedList{
     Node first;
     Node last;
@@ -20,53 +23,81 @@ public class MyLinkedList{
 	    if (i == index){
 		c.setValue(value);
 	    }
-	    c = c.next{};
+	    c = c.next();
 	}
     }
 
     public int size(){
-	return size;
+	return length;
     }
 
+    private class Node{
+	Node next, prev;
+	int data;
+	
+	public Node(Node n, Node p, int d){
+	    next = n;
+	    prev = p;
+	    data = d;
+	}
+	
+	public Node next(){
+	    return next;
+	}
+	
+	public void setNext(Node a){
+	    next = a;
+	}
+	
+	public Node prev(){
+	    return prev;
+	}
+	
+	public void setPrev(Node b){
+	    prev = b;
+	}
+	
+	public int getValue(){
+	    return data;
+	}
+	
+	public void setValue(int n){
+	    data = n;
+	}
+	
+	public String toString(){
+	    return "p:" + prev+ " c:" + data + " n:" + next;
+	}
+    }    
     
-
 }
 
-private class Node{
-    Node next, prev;
-    int data;
 
-    public Node(Node n, Node p, int d){
-	next = n;
-	prev = p;
-	data = d;
+/*
+public class Literator implements Iterator<String>{
+    private int current, end;
+    private Node v;
+
+    public Literator(int s, Node d, int e){
+      current = s;
+      end = e;
+      v = d;
     }
-    
+
+    public boolean hasNext(){
+      if (current <= end){
+        return true;
+      }
+      return false;
+    }
+
     public Node next(){
-	return next;
+      if (hasNext()){
+        current++;
+      }
+      else {
+        System.exit(0);
+      }
+      return v;
     }
-
-    public void setNext(Node a){
-	next = a;
-    }
-
-    public Node prev(){
-	return prev;
-    }
-
-    public void setPrev(Node b){
-	prev = b;
-    }
-
-    public int getValue(){
-	return data;
-    }
-
-    public void setValue(int n){
-	data = n;
-    }
-
-    public String toString(){
-	return "p:" + prev+ " c:" data + " n:" + next;
-    }
-}
+}*/
