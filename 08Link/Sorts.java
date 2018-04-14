706e070b443bd1e977101b9204725a9778dd2fd1
Ahmed.Imran <aimran@stuy.edu>
2018-04-14 00:12:48 -0400

    Success in sorting 1s into buckets

--- 08Link/Sorts.java
+++ 08Link/Sorts.java
@@ -6,15 +6,25 @@ public class Sorts{
 	@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] arr = new MyLinkedListImproved[10];
 	int length = data.size();
 	Integer max = data.max();
+	int dig = numDigits(max);
 	for (int i = 0; i < 10; i++){
 	    arr[i] = new MyLinkedListImproved<Integer>(); 
 	}
-	for (Integer i:data){
-	    
+	for (int n = 1; n <= dig; n++){
+	    for (Integer m:data){
+		Integer loc = m%10;
+		arr[loc].add(m);
+	    }
 	}
+
+	String ans = "";
+	for (int i = 0; i < 10;i++){
+	    ans += "" + i +arr[i] + "\n";
+	}
+	System.out.println(ans);
     }
 	
-    private static int numDigits(int num){
+    private static int numDigits(Integer num){
 	int  dig = 0;
 	while (num >= 1){
 	    dig++;
@@ -24,7 +34,13 @@ public class Sorts{
     }
 
     public static void main(String[] args){
-	System.out.println(numDigits(1234567891));
+	MyLinkedListImproved<Integer> l = new MyLinkedListImproved<>();
+	Random m = new Random();
+	for (int i = 0; i < 10;i++){
+	    l.add(m.nextInt(20));
+	}
+	System.out.println(l);
+	radixsort(l);
     }
 }
 
