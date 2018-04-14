3f41b337375b398fa9e5bf153a1e89bfd5a79749
Ahmed.Imran <aimran@stuy.edu>
2018-04-13 22:57:52 -0400

    Created buckets

--- 08Link/Sorts.java
+++ 08Link/Sorts.java
@@ -1,5 +1,15 @@
+import java.util.*;
+import java.io.*;
+
 public class Sorts{
     public static void radixsort(MyLinkedListImproved<Integer> data){
-
+	@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] arr = new MyLinkedListImproved[10];
+	for (int i = 0; i < 10; i++){
+	    arr[i] = new MyLinkedListImproved<Integer>(); 
+	}
     }
+
+    //  private static int numDigits(int num){
+	
+    //}
 }
