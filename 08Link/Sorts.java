c87fdfad1aca40146a9c7c59e0b200b16c05ad5b
Ahmed.Imran <aimran@stuy.edu>
2018-04-13 23:15:15 -0400

    Wrote method to find max digits

--- 08Link/Sorts.java
+++ 08Link/Sorts.java
@@ -4,12 +4,27 @@ import java.io.*;
 public class Sorts{
     public static void radixsort(MyLinkedListImproved<Integer> data){
 	@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] arr = new MyLinkedListImproved[10];
+	int length = data.size();
+	Integer max = data.max();
 	for (int i = 0; i < 10; i++){
 	    arr[i] = new MyLinkedListImproved<Integer>(); 
 	}
+	for (Integer i:data){
+	    
+	}
+    }
+	
+    private static int numDigits(int num){
+	int  dig = 0;
+	while (num >= 1){
+	    dig++;
+	    num = num / 10;
+	}
+	return dig;
     }
 
-    //  private static int numDigits(int num){
-	
-    //}
+    public static void main(String[] args){
+	System.out.println(numDigits(1234567891));
+    }
 }
+
