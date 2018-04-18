01d4ab8597a6bdf1564afe3c428405e03421d990
aimran <aimran@stuycs.org>
2018-04-18 15:16:32 -0400

    Added warning and constructor

--- 10Deq/MyDeque.java
+++ 10Deq/MyDeque.java
@@ -1,3 +1,11 @@
-public class MyDeque{
+import java.util.*;
+import java.io.*;
 
+public class MyDeque<E>{
+    private E[] data;
+    
+    @SuppressWarnings("unchecked")
+    public MyDeque(){
+	data = (E[])new Object[10];
+    }
 }
