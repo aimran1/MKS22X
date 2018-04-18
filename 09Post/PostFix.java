8512502ba42a767741a404a0fd64e4a4a3b60883
Ahmed.Imran <aimran@stuy.edu>
2018-04-17 22:48:48 -0400

    REmoved debug code

--- 09Post/PostFix.java
+++ 09Post/PostFix.java
@@ -10,16 +10,17 @@ public class PostFix{
 	LinkedList<Double> eq = new LinkedList<>();
 
 	for (String n: elements){
-	    System.out.println(n);
-	    	    System.out.println(eq);
 
 	    if (Character.isDigit(n.charAt(0))){
 		eq.push(Double.parseDouble(n));
 	    }
+	    
 	    else {
 		eq.push(operator(n,eq));
 	    }
+	    
 	}
+	
 	return eq.peek();
     }
 
@@ -28,24 +29,12 @@ public class PostFix{
 	double p1 = e.pop();
 
 	if (o.equals("+")){return p1 + p2;}
-	System.out.println("E: " + e);
-
 	if (o.equals("-")){return p1 - p2;}
-	System.out.println("E: " + e);
-
 	if (o.equals("/")){return p1 / p2;}
-	System.out.println("E: " + e);
-
 	if (o.equals("*")){return p1 * p2;}
-	System.out.println("E: " + e);
-
 	if (o.equals("%")){return p1 % p2;}
-	System.out.println("E: " + e);
-	return 0.0;
-    }
 
-    public static void main(String[] args){
-	System.out.println(	eval("8 2 + 99 9 - * 2 + 9 -"));
+	return 0.0;
     }
     
 }
