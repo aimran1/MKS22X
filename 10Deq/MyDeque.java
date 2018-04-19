fe19562d8a8508ba1b0b10b2b833995121efb786
aimran <aimran@stuycs.org>
2018-04-19 15:14:04 -0400

    Added second constructor

--- 10Deq/MyDeque.java
+++ 10Deq/MyDeque.java
@@ -12,6 +12,11 @@ public class MyDeque<E>{
     }
 
     @SuppressWarnings("unchecked")
+    public MyDeque(int initialCapacity){
+	data = (E[])new Object[initialCapacity];
+    }
+    
+    @SuppressWarnings("unchecked")
     private void resize(){
         E[] temp = (E[]) new Object[data.length*2];
 	for (int i = 0; i < data.length; i++){
@@ -19,5 +24,7 @@ public class MyDeque<E>{
 	}
 	data = temp;
     }
+
+    public 
 
 }
