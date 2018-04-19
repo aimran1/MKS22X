a02854ef3ced11845564b4020e9e9d266f0d69ae
aimran <aimran@stuycs.org>
2018-04-19 15:08:36 -0400

    Created resize

--- 10Deq/MyDeque.java
+++ 10Deq/MyDeque.java
@@ -3,9 +3,21 @@ import java.io.*;
 
 public class MyDeque<E>{
     private E[] data;
+    private int front;
+    private int back;
     
     @SuppressWarnings("unchecked")
     public MyDeque(){
 	data = (E[])new Object[10];
     }
+
+    @SuppressWarnings("unchecked")
+    private void resize(){
+        E[] temp = (E[]) new Object[data.length*2];
+	for (int i = 0; i < data.length; i++){
+	    temp[i] = data[i];
+	}
+	data = temp;
+    }
+
 }
