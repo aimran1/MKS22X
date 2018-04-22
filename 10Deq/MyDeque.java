15c8e6bb5dc7d925b0959decbec6998393d879ae
Ahmed.Imran <aimran@stuy.edu>
2018-04-22 17:54:32 -0400

    IT WORKS :)

--- 10Deq/MyDeque.java
+++ 10Deq/MyDeque.java
@@ -24,9 +24,12 @@ public class MyDeque<E>{
     private void resize(){
         E[] temp = (E[]) new Object[data.length*2];
 	if (front <= back){
-	    for (int i = front; i <= back; i++){
+	    int m = -1;
+	    for (int i = front; i <= back; i++, m++){
 		temp[i] = data[i];
 	    }
+	    front = 0;
+	    back = m;
 	}
 	else{
 	    int m = 0;
@@ -151,64 +154,6 @@ public class MyDeque<E>{
 	ans = ans.substring(0, ans.length() - 2) + "]";
 	return ans;
     }
-
-    /*   public static void main(String[] args){
-	MyDeque<Integer> l = new MyDeque<>();
-	for ( int i = 0; i < 12; i++){
-	    l.addFirst(i);
-	    System.out.println(l.getFirst());
-	    System.out.println(l);
-	}
-	System.out.println(l);
-	}*/
-
-    
-     public static void main(String[] args) {
-    MyDeque<String> a = new MyDeque<>(), a1 = new MyDeque<>();
-    ArrayList<String> b = new ArrayList<>();
-
-    int size = Integer.parseInt(args[0]);
-    for(int i = 0; i < size; i++){
-      int temp = (int)(Math.random() * 1000);
-      if(temp % 2 == 0){
-        a.addFirst("" + temp);
-        a1.addFirst("" + temp);
-        b.add(0, "" + temp);
-      }
-      else{
-        a.addLast("" + temp);
-        a1.addLast("" + temp);
-        b.add("" + temp);
-      }
-    }
-
-    int index = 0;
-    boolean hasError = false;
-    String errorEvaluation = "Errors found at these indices: ";
-    for (String x : b){
-      if (!(x.equals(a.getFirst()))){
-        System.out.println("The getFirst() function is incorrect at index " + index);
-        hasError = true;
-      }
-      if (!(x.equals(a.removeFirst()))){
-        System.out.println("There is an error at index " + index);
-        errorEvaluation += index + ", ";
-        hasError = true;
-      }
-      index++;
-    }
-
-
-    if(hasError){
-      errorEvaluation = errorEvaluation.substring(0, errorEvaluation.length() - 2);
-      System.out.println(errorEvaluation);
-      System.out.println("MyDeque: " + a1);
-      System.out.println("Actual Deque: " + b);
-    }
-    else{
-      System.out.println("Your deque is bug-free!");
-    }
-  }
 
 
 }
