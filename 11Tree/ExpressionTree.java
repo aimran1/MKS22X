8dea04c0d4e8ad5ca680512f7738db347a082da8
aimran <aimran@stuycs.org>
2018-04-24 15:12:12 -0400

    Wrote apply

--- 11Tree/ExpressionTree.java
+++ 11Tree/ExpressionTree.java
@@ -38,7 +38,26 @@ public class ExpressionTree{
     /*use the correct operator on both a and b, and return that value*/
     private double apply(char op, double a, double b){
 	/*you are to write this method*/
-	return 0.0;
+	double ans = 0;
+	if (op == '+'){
+	    ans = a + b;
+	}
+	else if(op == '-'){
+	    ans = a - b;
+	}
+	else if(op == '/'){
+	    ans = a / b;
+	}
+	else if(op == '*'){
+	    ans = a * b;
+	}
+	else if(op == '%'){
+	    ans = a % b;
+	}
+	else{
+	    System.exit(0);
+	}
+	return ans;
 	
     }
     
