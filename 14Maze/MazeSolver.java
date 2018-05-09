f9eb32c60263a2e15b7ba9b72d1dac24d34623f6
Ahmed.Imran <aimran@stuy.edu>
2018-05-08 22:01:47 -0400

    Compiles

--- 14Maze/MazeSolver.java
+++ 14Maze/MazeSolver.java
@@ -13,22 +13,44 @@ public class MazeSolver{
   //0: BFS
   //1: DFS
   public boolean solve(int mode){
-    //initialize your frontier
+
       if (mode == 0){
 	  frontier = new FrontierQueue();
       }
-    //while there is stuff in the frontier:
-    //  get the next location
-    //  process the location to find the locations (use the maze to do this)
-    //  check if any locations are the end, if you found the end just return true!
+      else if(mode == 1){
+	  frontier = new FrontierStack();
+      }
+      frontier.add(maze.getStart());
+      
       while (frontier.hasNext()){
 	  Location here = frontier.next();
-	  
+	  Location[] neighs = maze.getNeighbors(here);
+
+	  for (int i = 0; i < 4; i++){
+	      if (neighs[i].getX() == maze.getEnd().getX() &&
+		  neighs[i].getY() == maze.getEnd().getY()){
+		  return path(neighs[i]);
+	      }
+	      else{
+		  frontier.add(neighs[i]);
+		  maze.set(neighs[i].getX(), neighs[i].getY(), '?');
+	      }
+	  }
       }
-    //  add all the locations to the frontier
-    //when there are no more values in the frontier return false
+      
+      
     return false;
   }
+
+    private boolean path(Location loc){
+	while(loc.getX() != maze.getStart().getX() &&
+	      loc.getY() != maze.getStart().getY()){
+	    maze.set(loc.getX(),loc.getY(),'@');
+	    loc = loc.getPrev();
+	}
+	System.out.println(maze);
+	return true;
+    }
 
   public String toString(){
     return maze.toString();
